86901b5d4dc0ad2a778d32ca992a8dbd71cb8668
ashraf74 <ahmedashraaf09@gmail.com>
2025-03-29 04:23:14 +0200

    feat:configuring testng listener with ServiceLoader

--- src/test/java/com/automationexercise/tests/BaseTest.java
+++ src/test/java/com/automationexercise/tests/BaseTest.java
@@ -1,15 +1,13 @@
 package com.automationexercise.tests;
 
 import com.automationexercise.drivers.GUIDriver;
-import com.automationexercise.listeners.TestNGListeners;
 import com.automationexercise.utils.ConfigUtils;
 import com.automationexercise.utils.JsonUtils;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
 
-@Listeners(TestNGListeners.class)
+
 public class BaseTest {
 
     JsonUtils testData;
--- src/test/java/com/automationexercise/tests/E2e.java
+++ src/test/java/com/automationexercise/tests/E2e.java
@@ -1,16 +1,13 @@
 package com.automationexercise.tests;
 
 import com.automationexercise.drivers.GUIDriver;
-import com.automationexercise.listeners.TestNGListeners;
 import com.automationexercise.utils.ConfigUtils;
 import com.automationexercise.utils.JsonUtils;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 
-@Listeners(TestNGListeners.class)
 public class E2e {
 
     /// ///////////// Configurations \\\\\\\\\\\\\\\\\
